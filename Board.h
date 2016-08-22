292eafefbcf559da10a0a0c159c81d3520192d67
JinJie Wang <724895582@qq.com>
2016-08-22 11:12:24 +0800

    Add files via upload

--- Board.h
+++ Board.h
@@ -1,3 +1,4 @@
+
 #ifndef  _BOARD
 #define  _BOARD
 const int win = 7;              // 连五
@@ -10,11 +11,11 @@ const int block2 = 1;           // 眠二
 const int Ntype = 8;            // 棋型数量
 const int MaxSize = 20;         // 最大尺寸
 
-//棋子
-const int Empty=2;
-const int Black=1;
-const int White=0;
-//方向向量
+// 棋子
+const int Empty = 2;
+const int Black = 1;
+const int White = 0;
+// 方向向量
 const int dx[4] = { 1, 0, 1, 1 };
 const int dy[4] = { 0, 1, 1, -1 };
 
@@ -38,34 +39,83 @@ struct Cell {
 
 class Board {
 public:
-  int step = 0;                  //记录棋局步数
-  int size = 15;                 //棋盘当前尺寸
-  int typeTable[10][6][6][3];    //棋型对照表
-  Cell cell[MaxSize][MaxSize];   //棋盘结构，记录棋子颜色和棋型
-  Pos remMove[MaxSize * MaxSize];//记录每步棋的坐标
-  bool IsCand[MaxSize][MaxSize]; //记录每个位置是否合理着法（两格内有棋子）
-  bool IsLose[MaxSize][MaxSize]; //记录根节点的必败点
+  int step = 0;                 // 记录棋局步数
+  int size = 15;                // 棋盘当前尺寸
+  int typeTable[10][6][6][3];   // 棋型对照表
+  Cell cell[MaxSize][MaxSize];  // 棋盘结构，记录棋子颜色和棋型
+  Pos remMove[MaxSize * MaxSize]; // 记录每步棋的坐标
+  bool IsCand[MaxSize][MaxSize]; // 记录每个位置是否合理着法（两格内有棋子）
+  bool IsLose[MaxSize][MaxSize]; // 记录根节点的必败点
 
-  Board();
-  ~Board();
+    Board();
+   ~Board();
   void InitType();
-  void MakeMove(Pos next);
-  void DelMove();
   void Undo();
   void ReStart();
   void UpdateType(int x, int y);
   void UpdateRound(int n);
-  void TypeCount(int x, int y, int role, int *type);
-  bool CheckWin();
-  bool IsType(Pos p, int role, int type);
-  bool CheckXy(int x, int y) {
-    return (x >= 0 && x < size && y >= 0 && y < size);
-  }
+  int TypeLine(int role, int x, int y, int i, int j);
+  int ShortType(int role, int x, int y, int i, int j);
+  int GetType(int len, int len2, int count, int block);
+
+  /* 以下为可内联函数 */
   int color(int step) {
     return step % 2;
   }
-  int TypeLine(int role, int x, int y, int i, int j);
-  int ShortType(int role,int x,int y,int i,int j);
-  int GetType(int len, int len2, int count, int block);
+  // 判断点是否在棋盘内 
+  bool CheckXy(int x, int y) {
+    return x >= 0 && x < size && y >= 0 && y < size;
+  }
+
+  // 下子
+  void MakeMove(Pos next) {
+    int x = next.x;
+    int y = next.y;
+
+    ++step;
+    cell[x][y].piece = color(step);
+    remMove[step] = next;
+    UpdateRound(2);
+    UpdateType(x, y);
+  }
+
+  // 删子
+  void DelMove() {
+    int x = remMove[step].x;
+    int y = remMove[step].y;
+
+    --step;
+    cell[x][y].piece = Empty;
+    UpdateType(x, y);
+  }
+
+  // 记录四个方向棋型
+  void TypeCount(int x, int y, int role, int *type) {
+    Cell *c = &cell[x][y];
+    ++type[c->pattern[role][0]];
+    ++type[c->pattern[role][1]];
+    ++type[c->pattern[role][2]];
+    ++type[c->pattern[role][3]];
+  }
+
+  // 判断角色role在点p能否成棋型type
+  bool IsType(Pos p, int role, int type) {
+    Cell *c = &cell[p.x][p.y];
+    return c->pattern[role][0] == type
+      || c->pattern[role][1] == type
+      || c->pattern[role][2] == type 
+      || c->pattern[role][3] == type;
+  }
+
+  // 检查胜利
+  bool CheckWin() {
+    int role = color(step);
+    Cell *c = &cell[remMove[step].x][remMove[step].y];
+
+    return c->pattern[role][0] == win
+      || c->pattern[role][1] == win
+      || c->pattern[role][2] == win 
+      || c->pattern[role][3] == win;
+  }
 };
 #endif
