0a532f74a2c7c1c64d9419759469f4a2bca5e7e7
JinJie Wang <724895582@qq.com>
2016-08-28 00:27:12 -0500

    Update Board.h

--- Board.h
+++ Board.h
@@ -2,50 +2,51 @@
 #ifndef  _BOARD
 #define  _BOARD
 typedef unsigned long long U64;
-const int win = 7;              // ���
-const int flex4 = 6;            // ����
-const int block4 = 5;           // ����
-const int flex3 = 4;            // ����
-const int block3 = 3;           // ����
-const int flex2 = 2;            // ���
-const int block2 = 1;           // �߶�
-const int Ntype = 8;            // �������
-const int MaxSize = 20;         // ���ߴ�
-const int hashSize = 1 << 16;
+const int win = 7;              // 连五
+const int flex4 = 6;            // 活四
+const int block4 = 5;           // 冲四
+const int flex3 = 4;            // 活三
+const int block3 = 3;           // 眠三
+const int flex2 = 2;            // 活二
+const int block2 = 1;           // 眠二
+const int Ntype = 8;            // 棋型个数
+const int MaxSize = 20;         // 棋盘最大尺寸
+const int hashSize = 1 << 16;   // 哈希表尺寸
 
-// hash��־
+// hash表相关
 const int hash_exact = 0;
 const int hash_alpha = 1;
 const int hash_beta = 2;
 const int unknown = -20000;
 
-// ����
+// 点的状态
 const int Outside = 3;
 const int Empty = 2;
 const int Black = 1;
 const int White = 0;
-// �������
+// 方向向量
 const int dx[4] = { 1, 0, 1, 1 };
 const int dy[4] = { 0, 1, 1, -1 };
 
-// ���
+// 坐标
 struct Pos {
   int x;
   int y;
 };
 
-// �ŷ�
+// 带评价的点
 struct Point {
   Pos p;
   int val;
 };
 
-// ����״̬
+// 棋盘单点结构
 struct Cell {
   int piece;
   int pattern[2][4];
 };
 
+// 哈希表结构
 struct Hashe {
   U64 key;
   int depth;
@@ -56,18 +57,18 @@ struct Hashe {
 
 class Board {
 public:
-  int step = 0;                 // ��¼��ֲ���
-  int size = 15;                // ���̵�ǰ�ߴ�
-  int b_start, b_end;           // ���̱��������
-  U64 zobristKey = 0;
-  U64 zobrist[2][MaxSize + 4][MaxSize + 4];
-  Hashe hashTable[hashSize];
-  int typeTable[10][6][6][3];   // �����ͱ�
-  int patternTable[65536][2];   // �������ͱ�
-  Cell cell[MaxSize + 8][MaxSize + 8]; // ���̽ṹ����¼������ɫ������
-  Pos remMove[MaxSize * MaxSize]; // ��¼ÿ��������
-  bool IsCand[MaxSize + 8][MaxSize + 8]; // ��¼ÿ��λ���Ƿ�����ŷ�������������ӣ�
-  bool IsLose[25];              // ��¼��ڵ�ıذܵ�
+  int step = 0;                                 // 棋盘落子数
+  int size = 15;                                // 棋盘当前尺寸
+  int b_start, b_end;                           // 棋盘坐标索引
+  U64 zobristKey = 0;                           // 表示当前界面的zobristKey
+  U64 zobrist[2][MaxSize + 4][MaxSize + 4];     // zobrist键值表
+  Hashe hashTable[hashSize];                    // 哈希表
+  int typeTable[10][6][6][3];                   // 初级棋型表
+  int patternTable[65536][2];                   // 完整棋型表
+  Cell cell[MaxSize + 8][MaxSize + 8];          // 棋盘
+  Pos remMove[MaxSize * MaxSize];               // 记录落子
+  bool IsCand[MaxSize + 8][MaxSize + 8];        // 记录是否合理着法(两格以内)
+  bool IsLose[51];                              // 记录根节点的必败点
 
     Board();
    ~Board();
@@ -88,7 +89,7 @@ public:
   int ShortLine(int role, int *line);
   int GetType(int len, int len2, int count, int block);
 
-  /* ����Ϊ��������� */
+  /* 可内联成员函数 */
   int color(int step) {
     return step & 1;
   } bool CheckXy(int x, int y) {
