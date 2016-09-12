ab844ab991a5b088dc6496dfee52226bb9421ffd
JinJie Wang <724895582@qq.com>
2016-09-12 11:11:27 -0500

    Update AI.h

--- AI.h
+++ AI.h
@@ -31,7 +31,6 @@ public:
   int GetTime();
   int StopTime();
   int ScoreMove(int x, int y);
-  int ScorePoint(Cell * c, int role, int *b_type);
   int minimax(int depth, int alpha, int beta);
   int AlphaBeta(int depth, int alpha, int beta);
   int CutCand(Pos * move, Point * cand, int Csize);
