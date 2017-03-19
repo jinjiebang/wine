e02f6d6c91c8e43de8acfeca95d65411b0a3ca4e
JinJie Wang <724895582@qq.com>
2017-03-19 13:56:59 +0800

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -222,7 +222,7 @@ int Board::ShortLine(int *line) {
   int who = line[4];
   int opp = 1 - who;
   for (k = 5; k <= 8 && line[k] != opp; k++) {
-    if(line[k + 1] == opp){
+    if(k + 1 <= 8 && line[k + 1] == opp){
       block++;
     }
     if (line[k] == who) {
@@ -239,7 +239,7 @@ int Board::ShortLine(int *line) {
   // 计算中间空格
   kong = len2 - count;
   for (k = 3; k >= 0 && line[k] != opp; k--) {
-    if(line[k + 1] == opp){
+    if(k + 1 <= 8 && line[k + 1] == opp){
       block++;
     }
     if (line[k] == who) {
