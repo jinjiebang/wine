872a08f89f34cdde68e588dbe74cb56308cc5452
JinJie Wang <724895582@qq.com>
2017-04-04 14:06:27 +0800

    Update AI.cpp

--- AI.cpp
+++ AI.cpp
@@ -51,11 +51,8 @@ Pos AI::TurnBest() {
   Pos best = gobang();
   best.x -= 4, best.y -= 4;
   // 输出思考信息
-  cout << "MESSAGE";
-  cout << " depth=" << MaxDepth;
-  cout << " val=" << bestPoint.val;
-  cout << " NPS=" << total / (ThinkTime + 1) << "k";
-  cout << "[" << best.x << "," << best.y << "]" << endl;
+  cout << "MESSAGE" << " depth=" << MaxDepth << " NPS=" << total / (ThinkTime + 1) << "k" << endl;
+  cout << "MESSAGE" << " best: [" << best.x << "," << best.y << "]" << " val=" << bestPoint.val << endl;
   
   return best;
 }
